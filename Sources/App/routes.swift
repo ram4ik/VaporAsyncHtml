b62fd053736ae5679ba3bdaaf959e182b7602643
Ramill Ibragimov <ribragimov@malwarebytes.com>
2023-12-29 17:45:09 +0200

    logger

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,18 @@
 import Vapor
 
 func routes(_ app: Application) throws {
+    
+    app.logger.info("Setting up migrations...")
+    
+    let customLogger = Logger(label: "dev.customLogger.myAppLogs")
+    
     app.get { req async in
-        "It works!"
+        customLogger.info("Some info log, from custom logger")
+        return "It works!"
     }
 
     app.get("hello") { req async -> String in
-        "Hello, world!"
+        req.logger.info("Hello, logs!")
+        return "Hello, world!"
     }
 }
